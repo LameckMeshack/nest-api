885840b3fc2f5448b83f0e1ffb0c1e7f72455ddd
Lameck Meshack <rhinolamer@gmail.com>
2023-07-04 22:20:59 +0300

    Add validation and cfreated a post

--- src/user-module/user.controller.ts
+++ src/user-module/user.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserDto } from './dto/user.dto';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
-  @Get()
-  findAll(): string {
-    return this.userService.findAll();
-  }
+    constructor(private readonly userService: UserService) { }
+    @Get()
+    findAll(): string {
+        return this.userService.findAll();
+    }
+
+    @Post()
+    @UsePipes(new ValidationPipe())
+    createUser(@Body() user: UserDto) {
+        return this.userService.createUser(user);
+    }
 }
--- src/user-module/user.service.ts
+++ src/user-module/user.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { UserDto } from './dto/user.dto';
 
 @Injectable()
 export class UserService {
     findAll(): string {
         return 'TESTING ';
+    }
+    createUser(user: UserDto) {
+        return user;
     }
 }
