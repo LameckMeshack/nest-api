046aad4a657218b860a9ce7d2e5c090e90bc197b
LameckMeshack <otienolameck327@gmail.com>
2023-07-14 00:11:09 +0300

    If user exists prisma throw error

--- src/auth/auth.services.ts
+++ src/auth/auth.services.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2';
 import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 
 @Injectable({})
@@ -11,18 +12,36 @@ export class AuthService {
 
     async signup(regDto: AuthDto) {
         const hash = await argon.hash(regDto.password);
+        try {
+            // const user = await this.prisma.user.findUnique({
+            //     where: {
+            //         email: regDto.email
+            //     }
+            // });
+            // if (user) {
+            //     throw new Error('User already exists');
+            // }
 
-        const newUser = await this.prisma.user.create({
-            data: {
-                email: regDto.email,
-                hash,
+            const newUser = await this.prisma.user.create({
+                data: {
+                    email: regDto.email,
+                    hash,
+                }
+            });
+
+            // delete newUser.hash;
+
+            // console.log(newUser)
+            return newUser;
+        }
+        catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('User already exists');
+                }
             }
-        });
-
-        delete newUser.hash;
-
-        console.log(newUser)
-        return newUser;
+            throw new Error(error);
+        }
     }
 
     login() {
