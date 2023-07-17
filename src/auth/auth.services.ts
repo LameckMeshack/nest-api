67da9c012616a507b350b153d194414621837136
LameckMeshack <otienolameck327@gmail.com>
2023-07-17 23:14:57 +0300

    add jwt

--- src/auth/auth.services.ts
+++ src/auth/auth.services.ts
@@ -1,31 +1,31 @@
-import { ForbiddenException, Injectable } from "@nestjs/common";
-import { AuthDto } from "./dto";
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { AuthDto } from './dto';
 import * as argon from 'argon2';
-import { PrismaService } from "src/prisma/prisma.service";
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
-
+import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { config } from 'process';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable({})
 export class AuthService {
+    constructor(
+        private prisma: PrismaService,
+        private jwt: JwtService,
+        private config: ConfigService,
+    ) { }
 
-    constructor(private prisma: PrismaService) { }
-
-    async signup(regDto: AuthDto) {
+    async signup(regDto: AuthDto): Promise<{ access_token: string }> {
         const hashPass = await argon.hash(regDto.password);
         try {
             const newUser = await this.prisma.user.create({
                 data: {
                     email: regDto.email,
                     hash: hashPass,
-                }
+                },
             });
-
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
-            const { hash, ...regUser } = newUser;
-
-            return regUser;
-        }
-        catch (error) {
+            return this.signToken(newUser.id, newUser.email);
+        } catch (error) {
             if (error instanceof PrismaClientKnownRequestError) {
                 if (error.code === 'P2002') {
                     throw new ForbiddenException('User already exists');
@@ -35,25 +35,32 @@ export class AuthService {
         }
     }
 
-    async signin(loginDto: AuthDto) {
-
+    async signin(loginDto: AuthDto): Promise<{ access_token: string }> {
         const user = await this.prisma.user.findUnique({
-            where: { email: loginDto.email }
+            where: { email: loginDto.email },
         });
         if (!user) {
             throw new ForbiddenException('Credentials Incorrect');
         }
-
         const isMatch = await argon.verify(user?.hash, loginDto.password);
         if (!isMatch) {
             throw new ForbiddenException('Credentials Incorrect');
         }
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const { hash, ...authUser } = user;
-
-        return authUser;
+        return this.signToken(user.id, user.email);
     }
 
-
-
+    async signToken(
+        userId: number,
+        email: string,
+    ): Promise<{ access_token: string }> {
+        const payload = { sub: userId, email };
+        const secret = this.config.get<string>('JWT_SECRET');
+        const token = await this.jwt.signAsync(payload, {
+            expiresIn: '15m',
+            secret,
+        });
+        return {
+            access_token: token,
+        };
+    }
 }
