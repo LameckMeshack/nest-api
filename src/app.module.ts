3a69b4b4d5afa5abe98f30159fa35902e5c2784c
LameckMeshack <otienolameck327@gmail.com>
2023-07-17 22:18:11 +0300

    Add configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+
+
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
-import { PrismaModule } from './prisma/prisma.module';
+
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    // envFilePath: '.env',
 
-
-
+  }), AuthModule, UserModule, BookmarkModule, PrismaModule],
 })
 
 export class AppModule { }
